3ea25a57a8481f3ecb8e3e91bcfbdc265c1d7e31
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-05-03 02:18:26 +0100

    cloudflare pages config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  { rules: { "@typescript-eslint/no-explicit-any": "off" } },
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  'plugin:eslint-plugin-next-on-pages/recommended',
+  {
+    rules: { '@typescript-eslint/no-explicit-any': 'off' },
+    plugins: ['eslint-plugin-next-on-pages'],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
