2a68ed33d24cc50432578bfe86b2dbfad259a14c
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-05-12 11:41:03 +0100

    removed process.env

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export default async (req: NextRequest) => {
   const token: any = await getToken({
     req,
-    secret: process.env.AUTH_SECRET || 'bec56a29ab187ed64d8089197a577ac3',
+    secret: 'bec56a29ab187ed64d8089197a577ac3',
   })
   console.log(token)
   // const isLoggedIn = !!req.auth?.user
